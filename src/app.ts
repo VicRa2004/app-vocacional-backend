fdd96c6e13de5147a5b6129916d37d2448b1461c
Victor Raúl García García <victor07mejor@gmail.com>
2025-02-10 21:10:01 -0600

    Se agregaron las rutas

--- src/app.ts
+++ src/app.ts
@@ -9,10 +9,13 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 
+import routes from "./routes/index.routes";
+
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
+app.use(routes);
 
 export default app;
